4d6d8a64699fe704241524ed20f8401e08cbeef9
andre-alck <andrealck1@gmail.com>
2022-09-21 14:51:14 -0300

    fix: update receiver to handle async

--- lib/receiver_amqp.dart
+++ lib/receiver_amqp.dart
@@ -1,7 +1,7 @@
 import 'package:dart_amqp/dart_amqp.dart';
 import 'dart:io';
 
-void receive() async {
+Future<String> receive() async {
   Client client = Client();
   ProcessSignal.sigint.watch().listen((message) async {
     await client.close();
@@ -12,7 +12,12 @@ void receive() async {
   Queue queue = await channel.queue('topic');
   Consumer consumer = await queue.consume();
 
+  String messageReceived = "";
+
   consumer.listen((message) {
+    messageReceived = message.payloadAsString;
     print('message_received:\t${message.payloadAsString}');
   });
+
+  return messageReceived;
 }
